1fe2e86c3459c9a24d81c1313cf628e192be4afc
SaraG94 <sara.gherbassi.94@gmail.com>
2023-03-30 17:48:33 +0200

    FEAT function

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ class Movie{
     public $subtitle;
     public $vote;
     public $language;
+    public $releaseYear;
 
     //definire un costruttore
     function __construct($titleFilm,$subtitleFilm){
@@ -15,17 +16,29 @@ class Movie{
         $this->subtitle = $subtitleFilm;
     }
     //definire almeno un metodo
+    function getAgeRelease() {
+        
+        if(!isset($this->releaseYear)){
+          return "Anno uscita mancante";
+        }
+        
+        $annoCorrente = date("Y");    
+        return $annoCorrente - $this->releaseYear;
+    }
 }
-
+//film1
 $film1 = new Movie("Ritorno al futuro","grande giove");
 $film1->vote = 4.5;
 $film1->language ="eng";
+$film1->releaseYear = 1985; 
 
+//film2
 $film2 = new Movie("Il signore degli anelli","la compagnia dell'anello");
 $film2->vote = 5;
 $film2->language ="eng";
+$film2->releaseYear = 2001;
 
-var_dump($film1,$film2);
+var_dump($film1,$film1->getAgeRelease(),$film2, $film2->getAgeRelease());
 ?>
 
 
@@ -38,6 +51,6 @@ var_dump($film1,$film2);
     <title>php-oop-1</title>
 </head>
 <body>
-    
+   
 </body>
 </html>
